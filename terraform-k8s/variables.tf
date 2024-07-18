b0d8b5808724d3c088ec4a46954f24d67c7ddea7
your-username <dreamscaatcher@gmail.com>
2024-07-18 08:59:10 +0200

    chore: Add Terraform configuration for creating a Kubernetes cluster

---
+++ terraform-k8s/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  description = "The Google Cloud project ID"
+  default     = "eminent-yen-429707-d4"
+}
+
+variable "region" {
+  description = "The Google Cloud region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The Google Cloud zone"
+  default     = "us-central1-c"
+}
+
+
+
