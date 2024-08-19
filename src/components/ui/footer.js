0ddd87d3f374d9da1d386291b5db77b016fad085
gurinderghuman <gurinder@ghumanportfolio.com>
2024-08-19 11:42:07 +0200

    from vs code

--- src/components/ui/footer.js
+++ src/components/ui/footer.js
@@ -1,6 +1,6 @@
 // src/components/ui/footer.js
 const Footer = () => (
-    <footer className="bg-green-500 text-white py-4">
+    <footer className="bg-blue-500 text-white py-4">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} EcoFusion. All rights reserved.</p>
       </div>
