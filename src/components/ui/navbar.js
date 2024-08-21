cf432496675dd2d99ca6ba218a2f20a1da3d0982
gurinderghuman <gurinder@ghumanportfolio.com>
2024-08-21 12:31:54 +0200

    chore: Update navbar background color to green

--- src/components/ui/navbar.js
+++ src/components/ui/navbar.js
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 
 const Navbar = () => (
-  <nav className="bg-blue-900 p-4">
+  <nav className="bg-green-300 p-4">
     <div className="container mx-auto flex justify-between items-center">
       <div className="text-white text-2xl font-bold">EcoFusion</div>
       <div className="flex space-x-4">
